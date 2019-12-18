19a0de401633291ab5ba91e40eff06b03d4f79da
MateusMello1995 <mellowmateus@outlook.com>
2019-12-18 10:32:38 -0200

    criei as telas formulario e resultado

--- app/src/main/java/com/example/replicar/formulario.java
+++ app/src/main/java/com/example/replicar/formulario.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class page2 extends AppCompatActivity {
+public class formulario extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page2);
+        setContentView(R.layout.activity_formulario);
     }
     public void proximaTela(View view){
 
-        Intent intent = new Intent(this, formulario.class);
+        Intent intent = new Intent(this, resultado.class);
         startActivity(intent);
     }
 }
