36d0f969fd5bdbd6bc09d3a0c7ed09932064cbc2
MateusMello1995 <mellowmateus@outlook.com>
2019-12-19 08:35:46 -0300

    adicionado o codigo para enviar informações de uma tela à outra!

--- app/src/main/java/com/example/replicar/formulario.java
+++ app/src/main/java/com/example/replicar/formulario.java
@@ -5,8 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class formulario extends AppCompatActivity {
+    //declara atributo que vai receber o id do campo
+    public EditText nome;
+    public EditText email;
+    public EditText data;
+    public EditText telefone;
+    public EditText mensagem;
+    public EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +22,32 @@ public class formulario extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
     }
     public void proximaTela(View view){
+        //declara variaveis que vão pegar os campos
+        nome = findViewById(R.id.nome);
+        email = findViewById(R.id.email);
+        data = findViewById(R.id.data);
+        telefone = findViewById(R.id.telefone);
+        mensagem = findViewById(R.id.mensagem);
+        senha = findViewById(R.id.senha);
+
+        String nomeTexto = nome.getText().toString();
+        String emailTexto = email.getText().toString();
+        String dataTexto = data.getText().toString();
+        String telefoneTexto = telefone.getText().toString();
+        String mensagemTexto = mensagem.getText().toString();
+        String senhaTexto = senha.getText().toString();
 
         Intent intent = new Intent(this, resultado.class);
+
+        //envia campos para proxima tela
+        intent.putExtra("nome", nomeTexto);
+        intent.putExtra("email", emailTexto);
+        intent.putExtra("data", dataTexto);
+        intent.putExtra("telefone", telefoneTexto);
+        intent.putExtra("mensagem", mensagemTexto);
+        intent.putExtra("senha", senhaTexto);
+
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/replicar/resultado.java
+++ app/src/main/java/com/example/replicar/resultado.java
@@ -2,13 +2,53 @@ package com.example.replicar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class resultado extends AppCompatActivity {
+
+    //declara atributos que vão receber os campos
+    public TextView nomeCampo;
+    public TextView emailCampo;
+    public TextView dataCampo;
+    public TextView telefoneCampo;
+    public TextView mensagemCampo;
+    public TextView senhaCampo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        //verifica se foram passados os parametros na tela
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            //pega os valores passados na tela
+            String nome = bundle.getString("nome");
+            String email = bundle.getString("email");
+            String data = bundle.getString("data");
+            String telefone = bundle.getString("telefone");
+            String mensagem = bundle.getString("mensagem");
+            String senha = bundle.getString("senha");
+
+            //busca o campo na activity e adiciona o texto vindo de outra tela a ele
+            nomeCampo = findViewById(R.id.textViewNome);
+            nomeCampo.setText(nome);
+            emailCampo = findViewById(R.id.textViewEmail);
+            emailCampo.setText(email);
+            dataCampo = findViewById(R.id.textViewData);
+            dataCampo.setText(data);
+            telefoneCampo = findViewById(R.id.textViewTelefone);
+            telefoneCampo.setText(telefone);
+            mensagemCampo = findViewById(R.id.textViewMensagem);
+            mensagemCampo.setText(mensagem);
+            senhaCampo = findViewById(R.id.textViewSenha);
+            senhaCampo.setText(senha);
+
+        }
+
     }
 }
